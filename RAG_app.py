d7b3f0d76494cb56dcf43982a8741911851be8c0
shravanikn7890 <shravanikn456@gmail.com>
2026-04-17 11:26:25 +0530

    Fix image path formatting in RAG_app.py

--- RAG_app.py
+++ RAG_app.py
@@ -24,7 +24,7 @@ groq_api_key = st.secrets["GROQ_API_KEY"]
 
 # ------------------ UI ------------------
 st.set_page_config(page_title="Dynamic RAG with Groq", layout="wide")
-st.image(""C:\Users\prasad\Downloads\lord-krishna-playing-flute-with-divine-glow.jpg"")
+st.image("C:\Users\prasad\Downloads\lord-krishna-playing-flute-with-divine-glow.jpg")
 st.title("Dynamic RAG with Groq, FAISS, and Llama3")
 
 # ------------------ SESSION STATE ------------------
