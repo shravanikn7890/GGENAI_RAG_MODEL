a644050b5f9d09d7916521b5575c3ed5dfda2042
shravanikn7890 <shravanikn456@gmail.com>
2026-04-17 11:17:58 +0530

    Update RAG_app.py

--- RAG_app.py
+++ RAG_app.py
@@ -147,4 +147,18 @@ if prompt_input := st.chat_input("Ask a question about your documents..."):
         })
 
     else:
-        st.warning("Please process your documen
+        st.warning("Please process your documents before asking questions.")
+
+
+                
+
+                
+
+
+
+
+        
+
+               
+           
+       
